b7c35d82c95b25942b455fdae85a1baaff898f7e
vuhoangthienanh <132802424+vuhoangthienanh@users.noreply.github.com>
2023-06-18 10:26:13 +0700

    Update register.js

--- register.js
+++ register.js
@@ -16,4 +16,18 @@ function myFunction() {
     document.getElementById("dateInfo").innerHTML = localStorage.getItem("date");
     document.getElementById("genderInfo").innerHTML = localStorage.getItem("gender");
 }
+
+    let name = document.getElementById("name");
+    let pass = document.getElementById("pass");
+    let dob = document.getElementById("dob");
+    let gender = document.getElementById("gender");
+    if (name.value.length == 0 || pass.value.length == 0 || dob.value.length == 0 || gender.value == "Gender")
+    {
+        document.getElementById("thongbao").innerHTML = "Bạn cần nhập đầy đủ thông tin";
+    }
+    else 
+    {
+        document.getElementById("thongbao").innerHTML = "Bạn đã đủ thông tin";
+    }
+    
     
